e7b34b70eb71afcf94b8af3e0d511551277ef5db
Евгений <137326840+Belkub@users.noreply.github.com>
2025-12-12 10:32:42 +0300

    Add files via upload

---
+++ start.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.set_page_config(page_title = "This is a Multipage WebApp")
+st.title("Chart_mobile")
+st.write("Приложение для разработки ОГ")
+st.sidebar.success("Выбрать категорию")
