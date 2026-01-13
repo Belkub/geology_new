d396d15be351764dfdf94adbe81c10da5b699698
Евгений <137326840+Belkub@users.noreply.github.com>
2026-01-13 13:43:47 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,6 +1,9 @@
 import streamlit as st
-
+from PIL import Image
+img = Image.open("bent.png")
+st.image(img, width=150)
 st.set_page_config(page_title = "This is a Multipage WebApp")
 st.title("Chart_mobile")
 st.write("Приложение для разработки ОГ")
 st.sidebar.success("Выбрать категорию")
+
